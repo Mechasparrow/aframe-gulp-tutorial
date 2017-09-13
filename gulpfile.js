2701e538d07d9f115b9084aca7bc06a86b32632d
Michael Navazhylau <mikipux7@gmail.com>
2017-09-12 20:07:28 -0500

    added gulpfile and more packages

--- gulpfile.js
+++ gulpfile.js
@@ -1,44 +1,3 @@
-# Welcome to the A-frame Gulp.js Tutorial
-
-
-# Getting Started
-
-First we are going to install gulp and the helper libraries for it.
-
-Create a Node.Js Project
-
-``` npm init ```
-
-This will create a ``` package.json``` file
-
-Install Gulp.js  
-
-```
-npm install gulp-cli -g
-npm install gulp -D
-```
-
-Next we install helper libraries for Gulp
-
-``` npm install --save-dev jshint gulp-jshint gulp-concat gulp-uglify gulp-rename gulp-connect ```
-
-We are also going to install browserify to build it to web
-
-``` npm install --save-dev gulp-browserify ```
-
-Actually install Aframe.
-
-We should finally actually install aframe
-
-``` npm install --save aframe ```
-
-# Creating our build setup
-
-Create a file in the root of the project called ```gulpfile.js```
-
-Put this in it.
-
-```
 var gulp = require('gulp');
 
 var jshint = require('gulp-jshint');
@@ -84,7 +43,3 @@ gulp.task('connect', function () {
 
 // Default Task
 gulp.task('default', ['html', 'scripts', 'connect', 'watch']);
-
-```
-
-Alright awesome. Now to the actual code...
